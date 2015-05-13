824fcaedf406db293737e2859e0a2cc2370b7080
William Woodruff <william@tuffbizz.com>
2015-05-13 15:50:59 -0400

    commit @ Wed May 13 15:50:59 EDT 2015

--- textadept/init.lua
+++ textadept/init.lua
@@ -1,3 +1,17 @@
+-- Enable ulticursor editing
+require('common.multiedit')
+
+-- Enable multicursor editing
+buffer.multiple_selection = true
+buffer.additional_selection_typing = true
+buffer.additional_carets_visible = true
+
+-- Keybindings for multicursor editing
+local m_multiedit = _m.common.multiedit
+keys.cj = { m_multiedit.add_position }
+keys.cJ = { m_multiedit.add_multiple }
+keys.cr = { m_multiedit.selectAll }
+
 if not CURSES then
 	ui.set_theme('base16-monokai-dark-modified')
 end
@@ -9,12 +23,4 @@ textadept.file_types.patterns['^#!/usr/bin/env perl'] = 'perl'
 -- Add a vertical ruler at the 80th column
 buffer.edge_column = 80
 buffer.edge_mode = buffer.EDGE_LINE
-
--- Enable ulticursor editing
-require('common.multiedit')
--- Keybindings for multicursor editing
-local m_multiedit = _m.common.multiedit
-keys.cj = { m_multiedit.add_position }
-keys.cJ = { m_multiedit.add_multiple }
-keys.cr = { m_multiedit.selectAll }
 
