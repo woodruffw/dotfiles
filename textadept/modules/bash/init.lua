a5d4003c9308f67914efae46517f68f76a733a57
William Woodruff <william@tuffbizz.com>
2015-05-13 15:24:03 -0400

    more snippets testing

--- textadept/modules/bash/init.lua
+++ textadept/modules/bash/init.lua
@@ -3,5 +3,7 @@
 local M = {}
 
 snippets.bash = {
-    ['if'] = 'if [[ %1(condition) ]] ; then\n\t%2(body)\nfi'
+	['if'] = 'if [[ %1(cond) ]] ; then\n\t%2(body)\nfi',
+	['elif'] = 'elif [[ %1(cond) ]] ; then\n\t%2(body)\n',
+	['for'] = 'for %1(var) in %2(list) ; do\n\t%3(body)\ndone'
 }
