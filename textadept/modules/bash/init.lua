1f54c93e9fb1d54e6ad8c6eecf08e3aa68efdd75
William Woodruff <william@tuffbizz.com>
2015-05-13 15:27:26 -0400

    add func

--- textadept/modules/bash/init.lua
+++ textadept/modules/bash/init.lua
@@ -5,5 +5,6 @@ local M = {}
 snippets.bash = {
 	['if'] = 'if [[ %1(cond) ]] ; then\n\t%2(body)\nfi',
 	['elif'] = 'elif [[ %1(cond) ]] ; then\n\t%2(body)\n',
-	['for'] = 'for %1(var) in %2(list) ; do\n\t%3(body)\ndone'
+	['for'] = 'for %1(var) in %2(list) ; do\n\t%3(body)\ndone',
+	['func'] = 'function %1(func) {\n\t%2(body)\n}'
 }
