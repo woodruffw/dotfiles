d5409d27bdb3cec5d5152c2c7e782e74810e4ac5
William Woodruff <william@tuffbizz.com>
2015-05-13 15:34:52 -0400

    add ruler

--- textadept/init.lua
+++ textadept/init.lua
@@ -5,3 +5,6 @@ end
 textadept.file_types.patterns['^#!/usr/bin/env bash'] = 'bash'
 textadept.file_types.patterns['^#!/usr/bin/env ruby'] = 'ruby'
 textadept.file_types.patterns['^#!/usr/bin/env perl'] = 'perl'
+
+buffer.edge_column = 80
+buffer.edge_mode = buffer.EDGE_LINE
