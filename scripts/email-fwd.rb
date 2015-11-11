5743610f4e67e2a0af2fc8a687e3c5599deaa015
William Woodruff <william@tuffbizz.com>
2015-11-11 18:45:34 -0500

    no commit msg

--- scripts/email-fwd.rb
+++ scripts/email-fwd.rb
@@ -44,9 +44,8 @@ hash['emails'].each do |email, data|
 			envelope = m.attr['ENVELOPE']
 			text = m.attr['BODY[]']
 
-			smtp = Net::SMTP.new(data['smtp'], 25)
+			smtp = Net::SMTP.start(data['smtp'], 25, 'localhost', email, data['pass'], data['auth'].to_sym || :plain)
 			smtp.enable_starttls_auto
-			smtp.start("fwd", email, data['pass'], data['auth'].to_sym || :plain)
 
 			msg = <<-EOF.unindent
 				From: #{email}
