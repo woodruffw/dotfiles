16eedff6d9f405324d12e3da0a3330ecaecd1837
William Woodruff <william@tuffbizz.com>
2017-04-21 15:04:33 -0400

    bash_completion: Add bash completion for `snip`.

---
+++ bash_completion.d/snip.bash
@@ -0,0 +1,15 @@
+_snip_complete() {
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	case "${prev}" in
+		snip ) opts="help list new rm" ;;
+		rm ) opts=$(snip list) ;;
+	esac
+
+	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	return 0
+}
+
+complete -F _snip_complete snip
