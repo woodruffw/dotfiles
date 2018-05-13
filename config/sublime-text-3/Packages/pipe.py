9a20fd3c62477fb2ae3e7dc04d3832249e6753c3
William Woodruff <william@yossarian.net>
2018-05-13 17:31:33 -0400

    config/sublime-text-3: Add pipe plugin, keybinding

---
+++ config/sublime-text-3/Packages/pipe.py
@@ -0,0 +1,17 @@
+import sublime_plugin
+import shlex
+from subprocess import Popen, DEVNULL, PIPE
+
+
+class PipeCommand(sublime_plugin.TextCommand):
+    def run(self, _edit):
+        self.view.window().show_input_panel("Pipe:", "", self.pipe_command,
+                                            None, None)
+
+    def pipe_command(self, command):
+        args = shlex.split(command)
+        if not args:
+            return
+        popen = Popen(args, stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL)
+        output = popen.communicate()[0].decode("utf-8")
+        self.view.run_command("insert", {"characters": output})
