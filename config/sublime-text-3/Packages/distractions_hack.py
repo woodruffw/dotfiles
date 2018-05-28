1a2301353258873ba8d96c4cc2f79bbf50aa8226
William Woodruff <william@yossarian.net>
2018-05-28 00:02:38 -0400

    config/sublime-text-3: Add menu hiding to distractions_hack

--- config/sublime-text-3/Packages/distractions_hack.py
+++ config/sublime-text-3/Packages/distractions_hack.py
@@ -1,9 +1,11 @@
 import sublime
 import sublime_plugin
 
-def plugin_loaded():
-	windows = sublime.windows()
 
-	for w in windows:
-		w.set_minimap_visible(False)
-		w.set_status_bar_visible(False)
+def plugin_loaded():
+    windows = sublime.windows()
+
+    for w in windows:
+        w.set_minimap_visible(False)
+        w.set_status_bar_visible(False)
+        w.set_menu_visible(False)
