94e17ecfeb41d98ea36023b80d5d4731dd38f122
William Woodruff <william@yossarian.net>
2018-06-03 12:03:32 -0400

    config/sublime-text-3: Rewrite distractions_hack
    Now uses a ViewEventListener, rather than just `plugin_loaded`.

--- config/sublime-text-3/Packages/distractions_hack.py
+++ config/sublime-text-3/Packages/distractions_hack.py
@@ -1,11 +1,9 @@
-import sublime
 import sublime_plugin
 
 
-def plugin_loaded():
-    windows = sublime.windows()
-
-    for w in windows:
-        w.set_minimap_visible(False)
-        w.set_status_bar_visible(False)
-        w.set_menu_visible(False)
+class DistractionsHack(sublime_plugin.ViewEventListener):
+    def on_activated(self):
+        window = self.view.window()
+        window.set_minimap_visible(False)
+        window.set_status_bar_visible(False)
+        window.set_menu_visible(False)
